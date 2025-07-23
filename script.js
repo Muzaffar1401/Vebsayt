c87c1064f4194eff183c28f229564cd64d9f1b74
mmmm <marsitschool123@gmail.com>
2025-07-23 15:27:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = prompt('Ismgzn krting')
+
+let b = prompt('Bronta harf krting')
+
+
+    if(b == a){
+         alert('Bunday harf mavjud')
+    }else{
+        alert('Bunday harf mavjud emas')
+    }
